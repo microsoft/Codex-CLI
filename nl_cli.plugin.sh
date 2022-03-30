e1eb0d20a2681b9611959ae0ce7f2baed542aaa5
Dhruv Singh <dhrsingh@microsoft.com>
2022-03-30 09:36:30 -0700

    Revert "refactor + no-context functionality added"
    This reverts commit 88efc7662b4ee730b7810c6c49a167e59c4a412f.

--- nl_cli.plugin.sh
+++ nl_cli.plugin.sh
@@ -1,16 +1,6 @@
 create_completion() {
     # Get the text typed until now.
     text=${BUFFER}
-    completion=$(echo -n "$text" | $NL_CLI_PATH/codex_query.py -c)
-    # Add completion to the current buffer.
-    BUFFER="${text}${completion}"
-    # Put the cursor at the end of the line.
-    CURSOR=${#BUFFER}
-}
-
-create_completion_without_context() {
-    # Get the text typed until now.
-    text=${BUFFER}
     completion=$(echo -n "$text" | $NL_CLI_PATH/codex_query.py)
     # Add completion to the current buffer.
     BUFFER="${text}${completion}"
