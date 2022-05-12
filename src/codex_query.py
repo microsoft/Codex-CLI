28fde150dec378769af63cacecc9a075f9ecef53
Dhruv Singh <dhrsingh@microsoft.com>
2022-05-12 12:16:06 -0700

    forgot to turn off debugging mode

--- src/codex_query.py
+++ src/codex_query.py
@@ -19,7 +19,7 @@ ENGINE = ''
 TEMPERATURE = 0
 MAX_TOKENS = 300
 
-DEBUG_MODE = True
+DEBUG_MODE = False
 
 # api keys located in the same directory as this file
 API_KEYS_LOCATION = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'openaiapirc')
