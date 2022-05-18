10084d5571dee3a5cb2ce7104870fd477b153be7
Devis Lucato <dluc@users.noreply.github.com>
2022-05-18 14:46:04 -0700

    Fix bash scripts errors and support bash on macOS (#89)
    * Fix bash scripts errors and add bash support for macOS
    
    * Use user bash shell (env bash)
    * Prompt for settings when not passed as arguments
    * Support both direct bash script calls and script sourcing
    * Move bash settings to $HOME/.codexclirc, and don't rollback .bashrc on uninstall to avoid losing user changes
    * Remove dependency on git
    * Check for settings in bash plugin to avoid some error scenarios
    * Bind and unbind CTRL-g in the current shell after install/uninstall (if sourced)
    * Fix error message shown when the engine is not available
    * Add -d option to check for missing dependencies and help debugging
    * Update instructions and some docs with new name (full updates to be covered in separate PR)

--- scripts/bash_cleanup.sh
+++ scripts/bash_cleanup.sh
@@ -1,25 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#############################################
-## *** NL-CLI Cleanup Script for Bash *** ##
-#############################################
+uninstall()
+{
+    # Path to Codex CLI source
+    local CODEX_CLI_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+    # Path to OpenAI API settings
+    local OPENAI_RC_FILE="$CODEX_CLI_PATH/src/openaiapirc"
+    # Path to Bash settings loaded when a Bash session starts
+    local BASH_RC_FILE="$HOME/.codexclirc"
+    # Remove the plugin loaded by .bashrc
+    rm -f $BASH_RC_FILE
+    # Remove credentials and other personal settings
+    rm -f $OPENAI_RC_FILE
+    # Remove key binding (works only for sourced script calls)
+    if [ $SOURCED -eq 1 ]; then
+        bind -r "\C-g"
+    fi
 
-## *** Remove the openaiapirc file *** ##
-rm -f $NL_CLI_PATH/src/openaiapirc
+    echo "Codex CLI has been removed."
+}
 
-## *** Remove the env variables ***##
-unset NL_CLI_PATH
-unset BASH_NL_PATH
-unset ORG_ID
-unset ENGINE_ID
-unset SECRET_KEY
+# Detect if the script is sourced
+(return 0 2>/dev/null) && SOURCED=1 || SOURCED=0
 
+uninstall
 
-## *** Restore the original user bashrc file *** ##
-## *** Verify backup bashrc exists BEFORE removing and replacing bashrc ***##
-bkupBash=$HOME/.bashrc__
-currBash=$HOME/.bashrc
-if [ -f "$bkupBash" ]; then
-    rm -f $currBash
-    mv $bkupBash $currBash
-fi
+unset SOURCED
