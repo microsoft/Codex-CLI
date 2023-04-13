ae0f839c866e1c82404bdbe982428e89e5c7040f
Weltolk <40228052+Weltolk@users.noreply.github.com>
2023-04-13 17:00:01 +0800

    Update zsh_cleanup.sh

--- scripts/zsh_cleanup.sh
+++ scripts/zsh_cleanup.sh
@@ -9,7 +9,14 @@ openAIConfigPath="$CODEX_CLI_PATH/src/openaiapirc"
 zshrcPath="$HOME/.zshrc"
 
 # 1. Remove settings in .zshrc
-sed -i '' '/### Codex CLI setup - start/,/### Codex CLI setup - end/d' $zshrcPath
+# Check if the current system is Darwin (i.e. MacOS)
+if [[ "$OSTYPE" == "Darwin" ]]; then
+    # Command to execute on MacOS
+    sed -i '' '/### Codex CLI setup - start/,/### Codex CLI setup - end/d' $zshrcPath
+else
+    # Command to execute on Linux
+    sed -i '/### Codex CLI setup - start/,/### Codex CLI setup - end/d' $zshrcPath
+fi
 echo "Removed settings in $zshrcPath if present"
 
 # 2. Remove opanaiapirc in /.config
